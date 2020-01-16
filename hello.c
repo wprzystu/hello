91b13d3490da74ffe33a0edce8f8cd74af3cd62b
djeppesen <59979665+djeppesen@users.noreply.github.com>
2020-01-16 15:30:50 -0600

    I did the name and major addition.

--- hello.c
+++ hello.c
@@ -2,4 +2,7 @@ int main(int argc, char**argv) {
 	printf("william przystup\n);
 	printf("computer  Science\n");
 	return 0;
+	       printf("Daianera Jeppesen");
+	       printf("Computer Science Major");
+	       return 0;
 }
